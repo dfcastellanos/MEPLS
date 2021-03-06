05c483d7ec5f30f708318aa84cd2c43272142af0
David Fernández Castellanos <castellanos@gmx.com>
2021-03-06 15:29:00 +0100

    Create test suite for mepls

---
+++ test/main.cc
@@ -0,0 +1,39 @@
+// -----------------------------------------------------------------------
+//
+// Copyright (C) 2020  - David Fernández Castellanos
+//
+// This file is part of the MEPLS software. You can use it, redistribute
+// it, and/or modify it under the terms of the Creative Commons Attribution
+// 4.0 International Public License. The full text of the license can be
+// found in the file LICENSE at the top level of the MEPLS distribution.
+//
+// -----------------------------------------------------------------------
+
+#include <iostream>
+
+namespace mepls
+{
+
+namespace test
+{
+
+
+} // namespace test
+
+} // namespace mepls
+
+
+int main()
+{
+	std::cout << std::endl;
+	std::cout << "============================================" << std::endl;
+	std::cout << "================ MEPLS TESTS ===============" << std::endl;
+	std::cout << "============================================" << std::endl;
+
+
+	std::cout << "============================================" << std::endl;
+	std::cout << "=============== TESTS FINISHED =============" << std::endl;
+	std::cout << "============================================" << std::endl;
+
+	return 0;
+}
